6878369f8b333a7f7b5423696a980a9fbc7531e4
benaichouchemgit <benaichouchemgit@gmail.com>
2024-03-20 11:19:06 +0300

    don't send None

--- UI/pages/chat.py
+++ UI/pages/chat.py
@@ -35,13 +35,16 @@ components.iframe(CHAINLIT_APP, height=1000, width=1300, scrolling=False)
 # Call the CHAINLIT_APP endpoint with chatsettings to set the index_name and code execution
 
 
-index_name = st.sidebar.selectbox("Index name:", st.session_state.Indexes)
+index_name = st.sidebar.selectbox("Index name:",[""] + st.session_state.Indexes)
 code_execution = st.sidebar.selectbox("Code execution:", ["AssistantsAPI", "TaskWeaver"])
 top_results = st.sidebar.slider("Top results:",1,10,1 )
 
+value = ""
+if index_name:
+    value = index_name
 
 payload = {
-    "index_name": index_name,
+    "index_name":  value,
     "code_executor": code_execution,
     "top_results": top_results
 }
