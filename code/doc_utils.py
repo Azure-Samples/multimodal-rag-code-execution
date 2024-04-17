53ea902e7ff05c0543fc4c0e1de146db79ab14a3
samelhousseini <108335186+samelhousseini@users.noreply.github.com>
2024-04-17 10:34:16 +0400

    Fixing tenacity delay type

--- code/doc_utils.py
+++ code/doc_utils.py
@@ -237,7 +237,7 @@ def show_json(obj):
     display(json.loads(obj.model_dump_json()))
 
 
-@retry(wait=wait_random_exponential(min=1, max=30), stop=stop_after_delay(os.environ.get("TENACITY_STOP_AFTER_DELAY", 300)), after=after_log(logger, logging.ERROR))             
+@retry(wait=wait_random_exponential(min=1, max=30), stop=stop_after_delay(TENACITY_STOP_AFTER_DELAY), after=after_log(logger, logging.ERROR))             
 def get_chat_completion(messages: List[dict], model = AZURE_OPENAI_MODEL, client = oai_client, temperature = 0.2):
     print(f"\n{bc.OKBLUE}Calling OpenAI APIs:{bc.OKGREEN} {len(messages)} messages - {AZURE_OPENAI_MODEL} - {oai_client}\n{bc.ENDC}")
     return client.chat.completions.create(model = model, temperature = temperature, messages = messages, timeout=TENACITY_TIMEOUT)
@@ -251,7 +251,7 @@ def get_chat_completion_with_json(messages: List[dict], model = AZURE_OPENAI_MOD
 
 @retry(wait=wait_random_exponential(min=1, max=30), stop=stop_after_attempt(12), after=after_log(logger, logging.ERROR))     
 def get_embeddings(text, embedding_model = AZURE_OPENAI_EMBEDDING_MODEL, client = oai_emb_client):
-    return client.embeddings.create(input=[text], model=embedding_model,timeout=TENACITY_TIMEOUT).data[0].embedding
+    return client.embeddings.create(input=[text], model=embedding_model,timeout=TENACITY_TIMEOUT).data[0].embedding``
 
 
 def ask_LLM(prompt, temperature = 0.2, model_info = None):
