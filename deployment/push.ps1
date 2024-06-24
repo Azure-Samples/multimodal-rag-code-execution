92fe882eb8f5a768525a569439c71e9d347de664
Riccardo Chiodaroli <ricchi@microsoft.com>
2024-06-24 10:32:42 +0200

    fix: push images script

--- deployment/push.ps1
+++ deployment/push.ps1
@@ -6,19 +6,23 @@ param (
     [string]$ACR_NAME
 )
 
+# Ensure current working directory is parent of "deployment" folder
+$deploymentPath = (Get-Location).Path
+if ($deploymentPath -contains "deployment") {
+    Write-Host "Please run this script from the parent directory of the 'deployment' folder" -ForegroundColor Red
+    exit
+}
+
 # Generating a unique build tag using the current date and time
 $BUILD_ID = Get-Date -Format "yyyyMMddHHmmss"
 $TAG = "build-$BUILD_ID"
 
-# If $ACR_NAME is not provived, query the resource group for the ACR name not containing 'ml'
+# If $ACR_NAME is not provided, query the resource group for the ACR name not containing 'ml'
 if (-not $ACR_NAME) {
     $ACR_NAME = az acr list --resource-group $RG --query "[? !contains(name, 'ml')].name" -o tsv
 }
 
-write-host "Logging in to Azure Container Registry $ACR_NAME" -ForegroundColor Green
-# Logging in to Azure Container Registry and getting a token
-Write-Host "Attempting to log in to Azure Container Registry $ACR_NAME remotely..." -ForegroundColor Green
-az acr login --name $ACR_NAME --expose-token
+write-host "Using Azure Container Registry: $ACR_NAME"
 
 # Define paths to your Dockerfiles
 $DOCKERFILE_PATH_UI = "docker/dockerfile_chainlit_app"
@@ -30,36 +34,41 @@ $DOCKER_CUSTOM_IMAGE_NAME_UI = "$ACR_NAME.azurecr.io/research-copilot"
 $DOCKER_CUSTOM_IMAGE_NAME_MAIN = "$ACR_NAME.azurecr.io/research-copilot-main"
 $DOCKER_CUSTOM_IMAGE_NAME_API = "$ACR_NAME.azurecr.io/research-copilot-api"
 
+# Function to deploy web app
+function DeployWebApp($webAppName, $dockerImageName, $dockerFile, $sourceFolder, $tag) {
+
+    $confirmation = Read-Host -Prompt "Do you want to proceed with the build of $webAppName ? (Y/N)"
+    if ($confirmation -eq "Y" -or $confirmation -eq "y") {
+        $acrPassword = az acr credential show --name $ACR_NAME --resource-group $RG --query "passwords[0].value" -o tsv
+        Write-Host "Building the $webAppName Docker image using Azure Container Registry with tag $tag..." -ForegroundColor Green
+        az acr build --registry $ACR_NAME `
+            --resource-group $RG `
+            --image ${dockerImageName}:$tag `
+            --file $dockerFile $sourceFolder
+
+        Write-Host "Setting new Docker image for $webAppName ..." -ForegroundColor Green
+        az webapp config container set --name $webAppName `
+            --container-image-name "${dockerImageName}:$tag" `
+            --resource-group $RG `
+            --container-registry-url "https://$ACR_NAME.azurecr.io" `
+            --container-registry-user $ACR_NAME `
+            --container-registry-password $acrPassword
+
+        Write-Host "$dockerImageName build completed and deployed to Azure Web App $webAppName" -ForegroundColor Green
+    } else {
+        Write-Host "$dockerImageName build skipped by the user." -ForegroundColor Yellow
+    }
+}
+
+Write-Host "Fetching web app names from resource group $RG..."
 # Get all the web app names from resource group
-$CHAT_WEB_APP_NAME = az webapp list --resource-group $RG --query "[?contains(name, '-app-chat-')].name" -o tsv
-$MAIN_WEB_APP_NAME = az webapp list --resource-group $RG --query "[?contains(name, '-app-main-')].name" -o tsv
-$API_WEB_APP_NAME = az webapp list --resource-group $RG --query "[?contains(name, '-app-api-')].name" -o tsv
+$webAppNames = az webapp list --resource-group $RG --query "[?contains(name, '-app-')].name" -o tsv
+$CHAT_WEB_APP_NAME = $webAppNames | Where-Object { $_ -like '*-app-chat-*' }
+$MAIN_WEB_APP_NAME = $webAppNames | Where-Object { $_ -like '*-app-main-*' }
+$API_WEB_APP_NAME = $webAppNames | Where-Object { $_ -like '*-app-api-*' }
 
-# Ask for confirmation before building
-
-$confirmation = Read-Host -Prompt "Do you want to proceed with the build of chainlit app? (Y/N)"
-if ($confirmation -eq "Y" -or $confirmation -eq "y") {
-    Write-Host "Building the chainlit app Docker image using Azure Container Registry with tag $TAG..." -ForegroundColor Green
-    az acr build  --registry $ACR_NAME -g $RG --image ${DOCKER_CUSTOM_IMAGE_NAME_UI}:$TAG --image ${DOCKER_CUSTOM_IMAGE_NAME_UI}:latest --file $DOCKERFILE_PATH_UI ui
-    az webapp config container set --name $CHAT_WEB_APP_NAME --resource-group $RG --container-image-name ${DOCKER_CUSTOM_IMAGE_NAME_UI}:$TAG
-} else {
-    Write-Host "chainlit build cancelled by the user." -ForegroundColor Red
-}
-
-$confirmation = Read-Host -Prompt "Do you want to proceed with the build of streamlit? (Y/N)"
-if ($confirmation -eq "Y" -or $confirmation -eq "y") {    
-    Write-Host "Building the streamlit service Docker image using Azure Container Registry with tag $TAG..." -ForegroundColor Green
-    az acr build --registry $ACR_NAME -g $RG --image ${DOCKER_CUSTOM_IMAGE_NAME_MAIN}:$TAG --image ${DOCKER_CUSTOM_IMAGE_NAME_MAIN}:latest --file $DOCKERFILE_PATH_UI_MAIN ui
-    az webapp config container set --name $MAIN_WEB_APP_NAME --resource-group $RG --container-image-name ${DOCKER_CUSTOM_IMAGE_NAME_MAIN}:$TAG
-} else {
-    Write-Host "streamlit build cancelled by the user." -ForegroundColor Red
-}
-
-$confirmation = Read-Host -Prompt "Do you want to proceed with the build of API? (Y/N)"
-if ($confirmation -eq "Y" -or $confirmation -eq "y") {    
-    Write-Host "Building the API service Docker image using Azure Container Registry with tag $TAG..." -ForegroundColor Green
-    az acr build --registry $ACR_NAME -g $RG --image ${DOCKER_CUSTOM_IMAGE_NAME_API}:$TAG --image ${DOCKER_CUSTOM_IMAGE_NAME_API}:latest --file $DOCKERFILE_PATH_API code
-    az webapp config container set --name $API_WEB_APP_NAME --resource-group $RG --container-image-name ${DOCKER_CUSTOM_IMAGE_NAME_API}:$TAG
-} else {
-    Write-Host "API build cancelled by the user." -ForegroundColor Red
-}
+Write-Host "Web app names are: $CHAT_WEB_APP_NAME, $MAIN_WEB_APP_NAME, $API_WEB_APP_NAME"
+# Deploy web apps using the function
+DeployWebApp $CHAT_WEB_APP_NAME $DOCKER_CUSTOM_IMAGE_NAME_UI $DOCKERFILE_PATH_UI "ui" $TAG
+DeployWebApp $MAIN_WEB_APP_NAME $DOCKER_CUSTOM_IMAGE_NAME_MAIN $DOCKERFILE_PATH_UI_MAIN "ui" $TAG
+DeployWebApp $API_WEB_APP_NAME $DOCKER_CUSTOM_IMAGE_NAME_API $DOCKERFILE_PATH_API "code" $TAG
